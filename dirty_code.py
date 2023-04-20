6c3d78cce8aefc83c49ac529697483f9927b66a2
Shiva Heydari <shivaheydari.es@gmail.com>
2023-04-20 10:25:45 -0600

    Final list of seed  users and their friends

--- dirty_code.py
+++ dirty_code.py
@@ -5,10 +5,55 @@ import os
 
 ufile = open("Data/Galaxy_ds/valid_seed_users.txt")
 i = 0
-for uname in ufile:
-    uname = uname.strip()
-    # print(uname)
-    if os.path.isfile( 'Data/Galaxy_ds/followee/' + uname + '.txt') is False:
-        i+=1
-print(i)
+def get_list(source):
+
+    f = open(source, 'r')
+    friends= set()
+    for line in f:
+        line = line.strip()
+        friends.add(line)
+    return friends
+
+def savetofile(uname, friends):
+    f = open('Data/Galaxy_ds/Friends/'+uname+".txt", 'w')
+    for friend in friends:
+        f.write(friend+'\n')
+
+#Form the network
+
+# files = os.listdir('Data/Galaxy_ds/Friends')
+# f = open("Data/Galaxy_ds/valid_seed_users_v2.txt", 'w+')
+# for file in files:
+#     if file.endswith('.txt'):
+#         fname , txt = file.split(".txt")
+#         f.write(fname+'\n')
+
+
+#get the list of valid user:
+# valid_friends = open("Data/Galaxy_ds/valid_friends_users_v2.txt", 'w+')
+# users = open("Data/Galaxy_ds/valid_seed_users_v2.txt")
+# for user in users:
+#     user = user.rstrip()
+#     ufile = open("Data/Galaxy_ds/Friends/"+user+'.txt', 'r')
+#     for friend in ufile:
+#         friend = friend.rstrip()
+#         valid_friends.write(friend+'\n')
+
+valid_friends_unique =
+
+# for uname in ufile:
+#     uname = uname.strip()
+#     # print(uname)
+#     if os.path.isfile( 'Data/Galaxy_ds/follower/' + uname + '.txt') is True and os.path.isfile('Data/Galaxy_ds/followee/' + uname + '.txt') is True:
+#         #list of followers
+#         follower = get_list('Data/Galaxy_ds/follower/' + uname + '.txt')
+#         #list of followees
+#         followee = get_list('Data/Galaxy_ds/followee/' + uname + '.txt')
+#
+#         #intersection of these lists are friends
+#
+#         friends = followee.intersection(follower)
+#
+#         savetofile(uname, friends)
+
 
